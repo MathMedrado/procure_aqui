ea80e9eabf4be1b4983c00c4540663b388d8c468
Matheus Medrado <matheus-13-@hotmail.com>
2022-11-11 22:01:49 -0300

    quase nada hj

--- frontend/procure_aqui/lib/pages/productDetail.dart
+++ frontend/procure_aqui/lib/pages/productDetail.dart
@@ -18,21 +18,46 @@ class ProductDetailPage extends StatefulWidget {
 }
 
 class _ProductDetailPageState extends State<ProductDetailPage> {
+
+  @override
+  void initState(){
+    super.initState();
+    productData = fetchProductInfo();
+  }
+  late Future<Map> productData;
   final String category = 'Vegetal';
   final String supermarket = 'Comperfrutas';
   final String address = 'Av. Bernardo Sayão, 646 - Centro, Rialma - GO';
 
-  Future<Product> fetchUserInfo() async {
+  Future<Map> fetchProductInfo() async {
     // SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     // String? userEmail = sharedPreferences.getString('email');
-    var url = Uri.parse('http://10.0.2.2:8000/products/1');
+    var url = Uri.parse('http://10.0.2.2:8000/products/62/');
     var response = await http.get(url);
+    print(response.body);
 
     if(response.statusCode == 200){
-      return Product(id: jsonDecode(response.body)['id'], nameProduct: jsonDecode(response.body)['product_name'], barCode: jsonDecode(response.body)['bar_code'], categoryId: jsonDecode(response.body)['category_id'], imageUrl: jsonDecode(response.body)['image_url'], creationDate: jsonDecode(response.body)['creation_date_product'], isVisible: jsonDecode(response.body)['is_visible'], actualPrice: jsonDecode(response.body)['actual_price']);
+      return {
+        // 'id' : jsonDecode(response.body)['id'],
+        'nameProduct' : jsonDecode(response.body)['product_name'],
+        // 'barCode' : jsonDecode(response.body)['bar_code'],
+        'category' : jsonDecode(response.body)['category'],
+        // 'imageUrl': jsonDecode(response.body)['image_url'],
+        // 'creationDate' : jsonDecode(response.body)['creation_date'],
+        // 'isVisible' : jsonDecode(response.body)['is_visible'],
+        // //'actualPrice' : jsonDecode(response.body)['actual_price'],
+        'actualPrice' : 50.0,
+        // 'supermarket' : supermarket
+        'supermarket' : jsonDecode(response.body)['supermarket'][0]['supermarket_name'] ,
+        'supermarketAddress' : jsonDecode(response.body)['supermarket'][0]['street'],
+        'supermarketDistrict' : jsonDecode(response.body)['supermarket'][0]['district'],
+        'supermarketComplement' : jsonDecode(response.body)['supermarket'][0]['complement']
+      };
+      //return Product(id: jsonDecode(response.body)['id'], nameProduct: jsonDecode(response.body)['product_name'], barCode: jsonDecode(response.body)['bar_code'], categoryId: jsonDecode(response.body)['category_id'], imageUrl: jsonDecode(response.body)['image_url'], creationDate: jsonDecode(response.body)['creation_date_product'], isVisible: jsonDecode(response.body)['is_visible'], actualPrice: jsonDecode(response.body)['actual_price']);
     }
     else{
-      return Product(id: jsonDecode(response.body)['id'], nameProduct: jsonDecode(response.body)['product_name'], barCode: jsonDecode(response.body)['bar_code'], categoryId: jsonDecode(response.body)['category_id'], imageUrl: jsonDecode(response.body)['image_url'], creationDate: jsonDecode(response.body)['creation_date_product'], isVisible: jsonDecode(response.body)['is_visible'], actualPrice: jsonDecode(response.body)['actual_price']);
+      return {};
+     // return Product(id: jsonDecode(response.body)['id'], nameProduct: jsonDecode(response.body)['product_name'], barCode: jsonDecode(response.body)['bar_code'], categoryId: jsonDecode(response.body)['category_id'], imageUrl: jsonDecode(response.body)['image_url'], creationDate: jsonDecode(response.body)['creation_date_product'], isVisible: jsonDecode(response.body)['is_visible'], actualPrice: jsonDecode(response.body)['actual_price']);
     }
 
   }
@@ -44,122 +69,132 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
         preferredSize: Size.fromHeight(60.0),
         child: AppBarSearchFeature()
       ),
-      body:  SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 30),
-              child: Center(
-                child: Text(
-                  widget.product.getNameProduct,
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold
-                  ),
-                  textAlign: TextAlign.center,
-                )
-              ),
-            ),
-            Center(
-              child: Container(
-                margin: EdgeInsets.only(top: 35),
-                width: 257,
-                height: 200,
-                child: Image.asset(
-                  widget.product.getImageUrl,
-                  //'lib/assets/images/bolo.jpeg'
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 30, top: 20),
-              child: Text(
-                'Categoria: ${category}',
-                 style: TextStyle(fontSize: 18),
-              ),
-            ),
-            Row(
-              children: [
-                Container(
-                  margin: EdgeInsets.only( left: 30, top: 7),
-                  child: Text(
-                    'Preço: ',
-                    style: TextStyle(
-                      fontSize: 18,
+      body: FutureBuilder(
+        future: productData,
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Center(
+                        child: Text(
+                          snapshot.data!['nameProduct'],
+                          style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold
+                          ),
+                          textAlign: TextAlign.center,
+                        )
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.only( left: 2, top: 7),
-                  child: Text(
-                    'R\$ ${widget.product.getActualPrice.toString()}',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold
+                  Center(
+                    child: Container(
+                      margin: EdgeInsets.only(top: 35),
+                      width: 257,
+                      height: 200,
+                      child: Image.asset(
+                        //snapshot.data!['image_url'],
+                        'lib/assets/images/bolo.jpeg'
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 30, top: 7),
-              child: Text(
-                'Supermercado: ${supermarket}',
-                 style: TextStyle(fontSize: 18),
-              ),
-            ),
-            Container(
-              width: 330,
-              margin: EdgeInsets.only(left: 30, top: 7),
-              child: Text(
-                '${address}',
-                 style: TextStyle(fontSize: 18),
-              ),
-            ),
-            Container(
-              width: 160,
-              height: 35,
-              margin: EdgeInsets.only(left: 30, top: 25),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:  MaterialStatePropertyAll(Color(0xFF3700B3))
-                ),
-                child: Text('Adicionar a lista'),
-                onPressed: (){},
-              ),
-            ),
-            Container(
-              width: 330,
-              margin: EdgeInsets.only(left: 30, top: 7),
-              child: const Text(
-                '___________________________________',
-                 style: TextStyle(fontSize: 18),
-              ),
-            ),
-            Container(
-              width: 330,
-              margin: EdgeInsets.only(left: 30, top: 7, bottom: 20),
-              child: const Center(
-                child: Text(
-                  'Compare este produto em outros supermercados',
-                   style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
+                  Container(
+                    margin: EdgeInsets.only(left: 30, top: 20),
+                    child: Text(
+                      'Categoria: ${snapshot.data!['category']}',
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
-                  textAlign: TextAlign.center,
-                ),
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only( left: 30, top: 7),
+                        child: Text(
+                          'Preço: ',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only( left: 2, top: 7),
+                        child: Text(
+                          'R\$ ${snapshot.data!['actualPrice']}',
+                          style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 30, top: 7),
+                    child: Text(
+                      'Supermercado: ${snapshot.data!['supermarket']}',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    width: 330,
+                    margin: EdgeInsets.only(left: 30, top: 7),
+                    child: Text(
+                      '${snapshot.data!['supermarketAddress']}, ${snapshot.data!['supermarketDistrict']}, ${snapshot.data!['supermarketComplement']} ',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    width: 160,
+                    height: 35,
+                    margin: EdgeInsets.only(left: 30, top: 25),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:  MaterialStatePropertyAll(Color(0xFF3700B3))
+                      ),
+                      child: Text('Adicionar a lista'),
+                      onPressed: (){},
+                    ),
+                  ),
+                  Container(
+                    width: 330,
+                    margin: EdgeInsets.only(left: 30, top: 7),
+                    child: const Text(
+                      '___________________________________',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    width: 330,
+                    margin: EdgeInsets.only(left: 30, top: 7, bottom: 20),
+                    child: const Center(
+                      child: Text(
+                        'Compare este produto em outros supermercados',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  // otherMarketCard(product: widget.product),
+                  // otherMarketCard(product: widget.product),
+                  // otherMarketCard(product: widget.product),
+                  // otherMarketCard(product: widget.product)
+                ],
               ),
-            ),
-            otherMarketCard(product: widget.product),
-            otherMarketCard(product: widget.product),
-            otherMarketCard(product: widget.product),
-            otherMarketCard(product: widget.product)
-
-
-          ],
-        ),
-      ),
+            );
+          }
+          else{
+            final error = snapshot.error;
+            print(error);
+            return  CircularProgressIndicator();
+          }
+        },
+      )
     );
   }
 }
